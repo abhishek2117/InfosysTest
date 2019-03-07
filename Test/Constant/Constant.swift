010befd0c283330189f82402f420e771a00db5d3
Abhishek Rathore <abhishek2117@gmail.com>
2019-03-07 10:57:04 +0530

    WIP : Adding Constants.

---
+++ Test/Constant/Constant.swift
@@ -0,0 +1,17 @@
+//
+//  Constant.swift
+//  Test
+//
+//  Created by Champ on 07/03/19.
+//  Copyright © 2019 Infosys. All rights reserved.
+//
+
+import Foundation
+
+class Constant {
+    
+    static let countryCellIdentifier = "countryCellIdentifier"
+    
+    static let urlCountryDetail = "https://dl.dropboxusercontent.com/s/2iodh4vg0eortkl/facts.json"
+    
+}
